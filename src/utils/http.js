80cd3e72b8e86bceee992d128055b1b6bdde7bcc
lucerion <alexander.skachko@gmail.com>
2023-03-31 16:56:11 +0300

    Fix http urls

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,10 +1,12 @@
+const http = require('http');
 const https = require('https');
 
 const get = (url) => (
   new Promise((resolve) => {
     let data = '';
+    const client = new URL(url).protocol == 'https:' ? https : http;
 
-    https.get(url, (resp) => {
+    client.get(url, (resp) => {
       resp.on('data', (chunk) => data += chunk);
       resp.on('end', () => resolve(data));
     });
