c1ded24dc582485893260d0b1b094ac1337c81f6
lucerion <alexander.skachko@gmail.com>
2023-03-31 11:56:41 +0300

    Add page parsing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
 const express = require('express');
+const https = require('https');
+const { JSDOM } = require('jsdom');
+const { Readability } = require('@mozilla/readability');
+
 require('dotenv/config');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello, World!'));
+const request = (url) => (
+  new Promise((resolve) => {
+    let data = '';
+
+    https.get(url, (resp) => {
+      resp.on('data', (chunk) => data += chunk);
+      resp.on('end', () => resolve(data));
+    });
+  })
+);
+
+const layout = ({ title, content }) => (
+  `
+    <!doctype html>
+    <html>
+    <head>
+        <title>${title}</title>
+    </head>
+    <body>
+      ${content}
+    </body>
+    </html>
+  `
+);
+
+app.get('/', async (req, res) => {
+  const pageString = await request(req.query.url);
+  const pageDOM = new JSDOM(pageString);
+  const pageObject = new Readability(pageDOM.window.document).parse();
+  const readablePage = layout(pageObject);
+
+  return res.send(readablePage);
+});
 
 app.listen(port, () => console.warn(`Listening on port ${port}`));
