55de578219a9a6ba04ea3b02f50dff85408acfde
lucerion <alexander.skachko@gmail.com>
2023-04-02 08:11:56 +0300

    Move config validators to validators module

--- src/config.js
+++ src/config.js
@@ -1,15 +1,11 @@
+const { validateSecretKey, validateSecretIV } = require('./validators');
 require('dotenv/config');
 
 const PORT = process.env.PORT || 3000;
-
 const SECRET_KEY = process.env.SECRET_KEY;
-if (!SECRET_KEY) {
-  throw new Error('The `SECRET_KEY` environment variable is required but was not specified.');
-}
-
 const SECRET_IV = process.env.SECRET_IV;
-if (!SECRET_IV) {
-  throw new Error('The `SECRET_IV` environment variable is required but was not specified.');
-}
+
+validateSecretKey(SECRET_KEY);
+validateSecretIV(SECRET_IV);
 
 module.exports = { PORT, SECRET_KEY, SECRET_IV };
--- src/validators.js
+++ src/validators.js
@@ -1,3 +1,16 @@
+const validateSecretKey = (secretKey) => {
+  if (!secretKey) {
+    throw new Error('The `SECRET_KEY` environment variable is required but was not specified.');
+  }
+};
+
+const validateSecretIV = (secretIV) => {
+  if (!secretIV) {
+    throw new Error('The `SECRET_IV` environment variable is required but was not specified.');
+  }
+};
+
+// TODO: add validation that the url starts with 'http' or 'https'
 const isURLValid = (url) => {
   if (url) {
     return true;
@@ -14,4 +27,9 @@ const isURLHashValid = (hash) => {
   return false;
 };
 
-module.exports = { isURLValid, isURLHashValid };
+module.exports = {
+  validateSecretKey,
+  validateSecretIV,
+  isURLValid,
+  isURLHashValid,
+};
