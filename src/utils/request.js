f39ff030d754374eed7f662c84a9cd24636bc570
lucerion <alexander.skachko@gmail.com>
2023-04-05 04:57:01 +0300

    Follow redirects

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,13 +4,17 @@ const https = require('https');
 const client = (url) => new URL(url).protocol.startsWith('https') ? https : http;
 
 const get = (url) => (
-  new Promise((resolve) => {
+  new Promise((resolve, reject) => {
     let data = '';
 
     client(url).get(url, (resp) => {
+      if (resp.statusCode === 302) {
+        return resolve(get(resp.headers.location));
+      }
+
       resp.on('data', (chunk) => data += chunk);
       resp.on('end', () => resolve(data));
-    });
+    }).on('error', (error) => reject(error));
   })
 );
 
